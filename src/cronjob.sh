02c65eb969f83fbf6e56175a407488d3f0dcaae9
Jonas Bieri <jonas.bieri@gmail.com>
2018-05-30 10:57:20 +0000

    Optimize scripts

---
+++ src/cronjob.sh
@@ -0,0 +1,3 @@
+#This is the cronjob at cyon.ch to be run at least every 15 minutes. 
+#Append contents of each partial csv file (except the header line) to the complete csv file, then archive the partial files
+tail -q -n +2 /home/opendata/public_html/rues/onlinedaten/roh/*_RUES_Online_S3.csv >> /home/opendata/public_html/rues/onlinedaten/roh/2018_bis_aktuell.csv && mv /home/opendata/public_html/rues/onlinedaten/roh/*_RUES_Online_S3.csv /home/opendata/public_html/rues/onlinedaten/roh/archiv/
---
+++ src/reformat.sh
@@ -0,0 +1,2 @@
+#Replace tabs with semicolons
+tr '\t ' ';' < onlinedaten/roh/_2018_bis_aktuell.csv > onlinedaten/roh/2018_bis_aktuell.csv
