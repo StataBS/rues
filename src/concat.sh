29c8c8525caee6e420ed5f2c33e7cb351e0847ab
Jonas Bieri <jonas.bieri@gmail.com>
2018-06-04 14:42:38 +0000

    Optimize chart, add cron job to union all data

--- src/concat.sh
+++ src/concat.sh
@@ -1,3 +1,5 @@
 #Append contents of each partial csv file (except the header line) to the complete csv file, then archive the partial files
 tail -q -n +2 onlinedaten/roh/*_RUES_Online_S3.csv >> onlinedaten/roh/2018_bis_aktuell.csv && mv onlinedaten/roh/*_RUES_Online_S3.csv onlinedaten/roh/archiv/
 
+#Copy corrected data and append raw data to it
+cp onlinedaten/korrigiert/online2002_2017.csv onlinedaten/online2002_aktuell.csv &&  tail -q -n +2 onlinedaten/roh/2018_bis_aktuell.csv >> onlinedaten/online2002_aktuell.csv 
--- src/cronjob.sh
+++ src/cronjob.sh
@@ -1,3 +1,6 @@
 #This is the cronjob at cyon.ch to be run at least every 15 minutes. 
 #Append contents of each partial csv file (except the header line) to the complete csv file, then archive the partial files
 tail -q -n +2 /home/opendata/public_html/rues/onlinedaten/roh/*_RUES_Online_S3.csv >> /home/opendata/public_html/rues/onlinedaten/roh/2018_bis_aktuell.csv && mv /home/opendata/public_html/rues/onlinedaten/roh/*_RUES_Online_S3.csv /home/opendata/public_html/rues/onlinedaten/roh/archiv/
+
+#Copy corrected data and append raw data to it
+cp /home/opendata/public_html/rues/onlinedaten/korrigiert/online2002_2017.csv /home/opendata/public_html/rues/onlinedaten/online2002_aktuell.csv &&  tail -q -n +2 /home/opendata/public_html/rues/onlinedaten/roh/2018_bis_aktuell.csv >> /home/opendata/public_html/rues/onlinedaten/online2002_aktuell.csv 
